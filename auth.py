171210631d68c42188959abbbb84a56476a30abb
AJINKYA UBALE <vp2860083@gmail.com>
2026-09-23 23:21:22 +0530

    test: introduce broken validation logic in auth.py

--- auth.py
+++ auth.py
@@ -1,6 +1,3 @@
 def validate_user(username, password):
-    if not username or not password:
-        return False
-    if username == "admin" and password == "secret":
-        return True
     return False
+
